7709deec1391e939571201f6b26fe85c8982e9fd
NoCymer <61873313+NoCymer@users.noreply.github.com>
2024-06-04 15:21:28 +0200

    merge

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIcon, MatIconRegistry } from '@angular/material/icon'
 import { CardComponent } from './components/card/card.component';
 import { NavComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardComponent, NavComponent],
+  imports: [RouterOutlet, MatIcon, CardComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'client';
+  constructor(
+    private matIconReg: MatIconRegistry
+  ) {}
 }
