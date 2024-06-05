60a6c4beec1f874c883a999de4bc6830ee316c22
7nayr <rayan.mammeri8@gmail.com>
2024-06-05 15:10:28 +0200

    page accueil fini
    finalisé manque plus que la redirection du bouton connexion vers la page concerné

--- client/src/app/connexion.component.ts
+++ client/src/app/connexion.component.ts
@@ -1,20 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatIcon, MatIconRegistry } from '@angular/material/icon'
-import { CardComponent } from './components/card/PrimaryCard/primary-card.component';
 import { NavComponent } from './components/navbar_mobile/navbar.component';
-import { Card2Component } from './components/card/SecondaryCard/secondary-card.component.';
 import { HeaderComponent } from './components/header/header.component';
 
 
 @Component({
-  selector: 'app-root',
+  selector: 'connexion-root',
   standalone: true,
-  imports: [RouterOutlet, MatIcon, CardComponent, NavComponent, Card2Component, HeaderComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, MatIcon, NavComponent, HeaderComponent],
+  templateUrl: './connexion.component.html',
+  styleUrl: './connexion.component.scss'
 })
-export class AppComponent implements OnInit{
+export class ConnexionComponent implements OnInit{
   constructor(
     private matIconReg: MatIconRegistry
   ) {}
