fd11f1429a88015fe7edca5bdbf61783537e54e6
7nayr <rayan.mammeri8@gmail.com>
2024-06-11 17:43:25 +0200

    refactor

--- client/src/app/components/login-page/login-page.component.ts
+++ client/src/app/components/login-page/login-page.component.ts
@@ -1,12 +1,17 @@
 import { Component, Input } from "@angular/core";
 import { AuthService } from "../../services/Auth.service";
 import { Router } from "@angular/router";
+import { MatIcon } from "@angular/material/icon";
+import { NavbarMobileComponent } from "../navbar-mobile/navbar-mobile.component";
+import { MainHeaderComponent } from "../main-header/main-header.component";
+import { NavComponent } from "./login-card/login-card.component";
 
 @Component({
     selector: 'login-page',
     templateUrl: './login-page.component.html',
     styleUrl: './login-page.component.scss',
     providers: [AuthService],
+    imports: [MatIcon, NavbarMobileComponent, MainHeaderComponent, NavComponent],
     standalone: true,
 })
 export class LoginPage {
