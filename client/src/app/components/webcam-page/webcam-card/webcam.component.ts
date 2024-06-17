31d073ce7d766dbaa0e8c5a181892ba92cbac600
7nayr <rayan.mammeri8@gmail.com>
2024-06-17 11:03:24 +0200

    scan qui marche

--- client/src/app/components/webcam-page/webcam-card/webcam.component.ts
+++ client/src/app/components/webcam-page/webcam-card/webcam.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-webcam',
   templateUrl: './webcam.component.html',
   styleUrls: ['./webcam.component.scss'],
   standalone: true,
+  imports: [CommonModule]
 })
 export class WebcamComponent implements OnInit {
   @ViewChild('videoElement') videoElement!: ElementRef<HTMLVideoElement>;
+  capturedImage: string | null = null;
+  showScanButton: boolean = false; // Nouvelle propriété
 
   constructor() { }
 
@@ -27,28 +31,17 @@ export class WebcamComponent implements OnInit {
   }
 
   takePhoto() {
-    // Get the video element and canvas from DOM
     const video = this.videoElement.nativeElement;
     const canvas = document.createElement('canvas');
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
 
-    // Draw the video frame on the canvas
     const context = canvas.getContext('2d');
     if (context) {
       context.drawImage(video, 0, 0, canvas.width, canvas.height);
-      
-      // Convert canvas to base64 image data
-      const dataUrl = canvas.toDataURL('image/png');
-
-      // Optionally, you can save the dataUrl or send it to a server
-      console.log('Photo data URL:', dataUrl);
-      
-      // For now, let's just open the image in a new tab
-      const newTab = window.open();
-      if (newTab) {
-        newTab.document.body.innerHTML = `<img src="${dataUrl}" style="width: 100%;" />`;
-      }
+      this.capturedImage = canvas.toDataURL('image/png');
+      this.stopWebcam();
+      this.showScanButton = true; // Afficher le bouton scan
     }
   }
 
@@ -57,13 +50,24 @@ export class WebcamComponent implements OnInit {
     if (file) {
       const reader = new FileReader();
       reader.onload = (e: any) => {
-        // Display the imported image in a new tab
-        const newTab = window.open();
-        if (newTab) {
-          newTab.document.body.innerHTML = `<img src="${e.target.result}" style="width: 100%;" />`;
-        }
+        this.capturedImage = e.target.result;
+        this.stopWebcam();
+        this.showScanButton = true; // Afficher le bouton scan
       };
       reader.readAsDataURL(file);
     }
+  }
+
+  stopWebcam() {
+    const stream = this.videoElement.nativeElement.srcObject as MediaStream;
+    if (stream) {
+      const tracks = stream.getTracks();
+      tracks.forEach(track => track.stop());
+    }
+    this.videoElement.nativeElement.srcObject = null;
+  }
+
+  startScan() {
+    console.log('Scan started'); // Implémentez la logique du scan ici
   }
 }
